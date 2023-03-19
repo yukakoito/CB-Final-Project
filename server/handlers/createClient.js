7f3d1193e05da18f7d5abbb0528912b94d97c446
“Yukako <yito32@outlook.com>
2023-03-19 12:57:45 -0400

    Consolidate multiple db connections

--- server/handlers/createClient.js
+++ server/handlers/createClient.js
@@ -1,16 +1,44 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const { MONGO_URI } = process.env;
 
-const options = { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
+let client;
 
-const createClient = (dbName) => {
-    const client = new MongoClient(MONGO_URI, options);
-    const db = client.db(dbName);
-    return { client, db }
-}
+// Create client and connect to the database
+const createClient = () => {
+  try {
+    client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    client.connect();
+    console.log("Connected to the database");
+    return client.db("CBFinalProject");
+  } catch (err) {
+    throw new Error(
+      err?.message || "Unknown error occured during db connection"
+    );
+  }
+};
 
-module.exports = {createClient}
+// Close connection when the server is terminated
+const closeConnection = () => {
+  client.close((err) => {
+    console.log("Disconnected from the database");
+    // 1 - Exit with error
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    // 0 - Exit without error
+    process.exit(0);
+  });
+};
+
+// Disconnect when the server terminated by ctrl + c
+process.on("SIGINT", closeConnection);
+
+// Disconnect when the server terminated by kill command
+process.on("SIGTERM", closeConnection);
+
+module.exports = createClient();
