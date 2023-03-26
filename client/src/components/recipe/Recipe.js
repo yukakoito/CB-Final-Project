c1f34d690a90a33934d62810cb6b6b48555acfff
“Yukako <yito32@outlook.com>
2023-03-26 17:12:08 -0400

    Clean up recipe component by adding AllIngredients, RecipeIngredient and IngredientActionBar components

--- client/src/components/recipe/Recipe.js
+++ client/src/components/recipe/Recipe.js
@@ -1,68 +1,16 @@
-import { useContext, useState, useEffect, useRef } from "react";
+import { useContext, useState, useRef } from "react";
 import styled from "styled-components";
 import { UserContext } from "../../contexts/UserContext";
-import { HiOutlineViewList } from "react-icons/hi";
 import backupImage from "../../assets/noImage.png";
-import IconButton from "../IconButton";
 import Modal from "./Modal";
 import RecipeActionBar from "./RecipeActionBar";
+import AllIngredients from "./AllIngredients";
 
 const Recipe = ({ recipe, notes, isSavedRecipe }) => {
-  const { updateUser, pantry, shoppingList, userId, setRecipeToAdd } =
-    useContext(UserContext);
-  const [missingIngredients, setMissingIngredients] = useState([]);
-  const [availableIngredients, setAvailableIngredients] = useState([]);
-  const [hideAllIngredients, setHideAllIngredients] = useState(true);
-  const [hideIngredientsInPantry, setHideIngredientsInPantry] = useState(true);
+  const { userId, setRecipeToAdd } = useContext(UserContext);
   const [isImgErr, setIsImgErr] = useState(false);
   const imageRef = useRef();
   const [addRecipe, setAddRecipe] = useState(false);
-
-  const iconSize = 30;
-
-  // Find ingredients that user have at home
-  const compareIngredients = () => {
-    const newMissingIngredients = [];
-    const newAvailableIngredients = [];
-    Object.values(recipe.ingredients).forEach((ingredient) => {
-      const availableIngredient = pantry.find(
-        (item) => item.name.toLowerCase() === ingredient.food.toLowerCase()
-      );
-      const isInShoppingList = shoppingList.find(
-        (item) => item.name.toLowerCase() === ingredient.food.toLowerCase()
-      )
-        ? true
-        : false;
-      const missingIngredient = {
-        name: ingredient.food.toLowerCase(),
-        category: ingredient.foodCategory?.toLowerCase(),
-        isInShoppingList: isInShoppingList,
-      };
-      if (
-        availableIngredient &&
-        !newAvailableIngredients.find(
-          (obj) =>
-            obj.name.toLowerCase() === availableIngredient.name.toLowerCase()
-        )
-      ) {
-        newAvailableIngredients.push(availableIngredient);
-      } else if (
-        !newMissingIngredients.find(
-          (obj) =>
-            obj.name.toLowerCase() === "water" ||
-            obj.name.toLowerCase() === missingIngredient.name.toLowerCase()
-        )
-      ) {
-        newMissingIngredients.push(missingIngredient);
-      }
-    });
-    setAvailableIngredients(newAvailableIngredients);
-    setMissingIngredients(newMissingIngredients);
-  };
-
-  useEffect(() => {
-    recipe && compareIngredients();
-  }, [recipe, pantry, shoppingList]);
 
   // Capitalise every first letter of each word of cuisineType
   const formatCuisineType = (data) => {
@@ -123,89 +71,7 @@ const Recipe = ({ recipe, notes, isSavedRecipe }) => {
             recipe={recipe}
             setAddRecipe={setAddRecipe}
           />
-          <IngredientWrapper>
-            <div>
-              <h3>All Ingredients ({recipe.ingredientLines.length})</h3>
-              <IconButton
-                onClickFunc={setHideAllIngredients}
-                data={!hideAllIngredients}
-              >
-                <HiOutlineViewList size={iconSize} />
-              </IconButton>
-            </div>
-            {!hideAllIngredients &&
-              recipe.ingredientLines &&
-              recipe.ingredientLines.map((ingredient, i) => (
-                <li key={`${recipe.label}-allIngredient-${i}-${ingredient}`}>
-                  {ingredient}
-                </li>
-              ))}
-          </IngredientWrapper>
-          {userId && (
-            <IngredientWrapper>
-              <div>
-                <h3>You have {availableIngredients.length} ingredient(s)</h3>
-                <IconButton
-                  onClickFunc={setHideIngredientsInPantry}
-                  data={!hideIngredientsInPantry}
-                >
-                  <HiOutlineViewList size={iconSize} />
-                </IconButton>
-              </div>
-              {!hideIngredientsInPantry && (
-                <>
-                  {availableIngredients.map((ingredient) => (
-                    <li
-                      key={`${recipe.label}-availableIngredients-${ingredient.name}`}
-                    >
-                      {ingredient.name}
-                    </li>
-                  ))}
-                  <h3>Do you have any of the following ingredients?</h3>
-                  {missingIngredients &&
-                    missingIngredients.map((ingredient, i) => (
-                      <section
-                        key={`${recipe.label}-missingIngredients-${ingredient.name}`}
-                      >
-                        <li>{ingredient.name}</li>
-                        <button
-                          onClick={() =>
-                            updateUser({
-                              pantry: Object.fromEntries(
-                                Object.entries(ingredient).filter(
-                                  ([key, value]) => key !== "isInShoppingList"
-                                )
-                              ),
-                            })
-                          }
-                        >
-                          ➕ Pantry
-                        </button>
-                        <button
-                          onClick={() =>
-                            updateUser({
-                              shoppingList: Object.fromEntries(
-                                Object.entries(ingredient).filter(
-                                  ([key, value]) => key !== "isInShoppingList"
-                                )
-                              ),
-                            })
-                          }
-                          disabled={ingredient.isInShoppingList ? true : false}
-                          style={
-                            ingredient.isInShoppingList
-                              ? { opacity: "0.3" }
-                              : null
-                          }
-                        >
-                          ➕ Shopping List
-                        </button>
-                      </section>
-                    ))}
-                </>
-              )}
-            </IngredientWrapper>
-          )}
+          <AllIngredients recipe={recipe} />
         </RecipeFooter>
       </Wrapper>
     )
---
+++ client/src/components/recipe/RecipeIngredient.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { useState, useContext, useEffect } from "react";
+import IngredientActionBar from "./IngredientActionBar";
+import { UserContext } from "../../contexts/UserContext";
+
+const RecipeIngredient = ({ ingredient, availableIngredients }) => {
+  const [isInPantry, setIsInPantry] = useState(false);
+  const [isInShoppingList, setIsInShoppingList] = useState(false);
+  const { pantry, shoppingList } = useContext(UserContext);
+  const { text, food, foodCategory } = ingredient;
+
+  const item = { name: food.toLowerCase(), category: foodCategory };
+
+  const checkIsListed = (list) => {
+    const listName = Object.keys(list)[0];
+    const listItems = Object.values(list)[0];
+
+    const isIncluded = listItems.some(
+      (listItem) =>
+        listItem.name.trim().toLowerCase() === food.trim().toLowerCase()
+    );
+
+    listName === "pantry"
+      ? setIsInPantry(isIncluded)
+      : setIsInShoppingList(isIncluded);
+  };
+
+  useEffect(() => {
+    checkIsListed({ pantry: availableIngredients });
+  }, [availableIngredients]);
+
+  useEffect(() => {
+    checkIsListed({ shoppingList });
+  }, [shoppingList]);
+
+  return (
+    <Wrapper>
+      <span>{text}</span>
+      {pantry && (
+        <IngredientActionBar
+          item={item}
+          isInPantry={isInPantry}
+          isInShoppingList={isInShoppingList}
+        />
+      )}
+    </Wrapper>
+  );
+};
+
+export default RecipeIngredient;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 0;
+  border-bottom: 1px solid lightgray;
+
+  span {
+    overflow-wrap: anywhere;
+    margin-right: 5px;
+  }
+`;
